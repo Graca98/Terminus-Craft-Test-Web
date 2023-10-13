8fd52af19ca20d759afe2977ab929f6c3f40eb8e
Grácouš <84779291+Graca98@users.noreply.github.com>
2023-10-13 08:50:37 +0200

    Přidání uderline effect a test import/export

--- js/index.js
+++ js/index.js
@@ -4,19 +4,6 @@ function nactiPopovers() {
   const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 }
 
-// Zkopíruje text buttonu
-function copyIp() {
-  let copyIp = document.getElementById("serverIP");
-  navigator.clipboard.writeText(copyIp.innerText)
-  // Popover zmizí za 3s
-  setTimeout(function() {
-    const popover = bootstrap.Popover.getInstance(copyIp);
-    if (popover) {
-      popover.hide();
-    }
-  }, 3000);
-}
-
 
 // Dropwodn se zavře
 $('.dropdown-item').on('click', function(){
@@ -25,9 +12,6 @@ $('.dropdown-item').on('click', function(){
 $(document).ready(function(){
 })
 
-
-
-// const cilovyDiv = document.getElementById('obsahStranky');
 
 //! Funkce pro načtení obsahu z hlavni.html
 const cilovyDiv = document.getElementById('obsahStranky');
@@ -94,26 +78,39 @@ if (cilovyDiv.innerHTML === "") {
 //* Test zone
 let btnMain = document.getElementById("btnMain")
 let btnServers = document.getElementById("btnServers")
+let btnTeam = document.getElementById("btnTeam")
+let btnVyhody = document.getElementById("btnVyhody")
+let btnFaq = document.getElementById("btnFaq")
+let btnBany = document.getElementById("btnBany")
 
 function removeActivePages(id) {
   if (btnMain.classList.contains("active")) {
     btnMain.classList.remove("active")
   } else if (btnServers.classList.contains("active")) {
     btnServers.classList.remove("active")
+  } else if (btnTeam.classList.contains("active")) {
+    btnTeam.classList.remove("active")
+  } else if (btnVyhody.classList.contains("active")) {
+    btnVyhody.classList.remove("active")
+  } else if (btnFaq.classList.contains("active")) {
+    btnFaq.classList.remove("active")
+  } else if (btnBany.classList.contains("active")) {
+    btnBany.classList.remove("active")
   }
 }
 
+// Po přídání divu s obsahem textu je potřeba ho přidat i sem pro active page underline
 function activePage(divId) {
-  console.log(divId + "div znám")
+  console.log("Div " + divId + " nalezen")
   if (divId) {
-    console.log("prvni část")
-    if (divId === "informace" || divId === "pravidla") {
-      console.log("druhá část")
-      btnMain.classList.add("active")
-    } else if (divId === "servers" || divId === "serverSky") {
+    if (divId === "informace" || divId === "pravidla" || divId === "download" || divId === "kontakt") {
+      btnMain.classList.add("active") 
+    } else if (divId === "servers" || divId === "serverSky" || divId === "serverSur") {
       btnServers.classList.add("active")
+    } else if (divId === "aTeam") {
+      btnTeam.classList.add("active")
     } else {
-      console.error("Nenalezen žádný button na active page")
+      console.error("Nenalezen button na active page")
     }
   } else {
     console.error(`Element s ID "${divId}" nebyl nalezen.`);
